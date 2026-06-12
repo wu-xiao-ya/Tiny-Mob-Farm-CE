ecbc81ffea0c2a275478f0888d770dffced5b22f
兀小雅 <zpyn1818@outlook.com>
2026-06-12 15:53:14 +0800

    Rename mod display name to Tiny Mob Farm CE

--- src/main/java/cn/davidma/tinymobfarm/core/Reference.java
+++ src/main/java/cn/davidma/tinymobfarm/core/Reference.java
@@ -2,7 +2,7 @@ package cn.davidma.tinymobfarm.core;
 
 public class Reference {
 
-	public static final String MOD_NAME = "Tiny Mob Farm Community Edition";
+	public static final String MOD_NAME = "Tiny Mob Farm CE";
 	public static final String MOD_ID = "tinymobfarm";
 	public static final String VERSION = "1.0.6-ce";
 	public static final String MC_VERSION = "[1.12.2]";
